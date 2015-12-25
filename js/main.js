10c2a8b74d2fff327ff20db56809d68b663bf59d
Melissa Wilcox <melissa@emdubb.co>
2015-12-25 05:46:19 -0800

    add jquery animations

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('body').fadeIn(1500)
+    .removeClass('hidden')
+});
+
+$(window).one('scroll', function(){
+    $('body').css('background-image', 'url(../img/parallaxbg.jpg)');
+});
+
+$(window).one('scroll', function(){
+    $('.top').fadeIn('slow', function(){
+
+    });
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
