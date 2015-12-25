4de37b9d47b3e5dea9b4ff384dae0021711c6094
Melissa Wilcox <melissa@emdubb.co>
2015-12-25 02:57:40 -0800

    add sort functionality

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -3,12 +3,51 @@
 
   angular
     .module("emdubb")
-    .controller("HomeController", HomeController);
+    .controller("HomeController", HomeController)
+    .filter("workFilter", workFilter);
 
   HomeController.$inject = ["$log", "$state", "workDataService"];
 
   function HomeController($log, $state, workDataService) {
     var vm = this;
     vm.work = workDataService;
+    vm.setGroup = setGroup;
+
+    vm.selectedGroup = '';
+
+    function setGroup(group) {
+      if (group === 1) {
+        vm.selectedGroup = "development";
+      } else if (group === 2) {
+        vm.selectedGroup = "interactive";
+      } else if (group === 3) {
+        vm.selectedGroup = "print";
+      } else {
+        vm.selectedGroup = "all";
+      }
+    }
+  }
+
+  workFilter.$inject = ["$log"];
+
+  function workFilter($log){
+    return function(values, category) {
+      if(!category) {
+        // initially don't filter
+        return values;
+      }
+      // filter when we have a selected groupId
+      return values.filter(function(value){
+        if (category === "development") {
+          return value.development === true;
+        } else if (category === "interactive") {
+          return value.interactive === true;
+        } else if (category === "print") {
+          return value.print === true;
+        } else {
+          return value;
+        }
+      });
+    }
   }
 })();
--- js/work.dataservice.js
+++ js/work.dataservice.js
@@ -9,34 +9,84 @@
 
   function workDataService() {
     var item1 = {
-      title: "Project1",
+      title: "PiRS",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: false
     }
     var item2 = {
-      title: "Project2",
+      title: "Tsunami Campaign",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: true
     }
     var item3 = {
-      title: "Project3",
+      title: "What's Slackin'",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: false
     }
     var item4 = {
-      title: "Project4",
+      title: "Up the River",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: false
     }
     var item5 = {
-      title: "Project5",
+      title: "RAVE Wireless",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: false
     }
     var item6 = {
-      title: "Project6",
+      title: "RAVE Centric",
       thumbUrl: "../img/thumbs/thumb.jpg",
-      body: "lorem ipsum"
+      body: "lorem ipsum",
+      development: false,
+      interactive: true,
+      print: false
+    }
+    var item7 = {
+      title: "Gorilla Therapy",
+      thumbUrl: "../img/thumbs/thumb.jpg",
+      body: "lorem ipsum",
+      development: true,
+      interactive: true,
+      print: true
+    }
+    var item8 = {
+      title: "Say What?",
+      thumbUrl: "../img/thumbs/thumb.jpg",
+      body: "lorem ipsum",
+      development: false,
+      interactive: false,
+      print: true
+    }
+    var item9 = {
+      title: "Negretude",
+      thumbUrl: "../img/thumbs/thumb.jpg",
+      body: "lorem ipsum",
+      development: false,
+      interactive: false,
+      print: true
+    }
+    var item10 = {
+      title: "Invisible Children",
+      thumbUrl: "../img/thumbs/thumb.jpg",
+      body: "lorem ipsum",
+      development: false,
+      interactive: false,
+      print: true
     }
 
     var work = [
@@ -45,7 +95,11 @@
       item3,
       item4,
       item5,
-      item6
+      item6,
+      item7,
+      item8,
+      item9,
+      item10
     ]
 
     return work
