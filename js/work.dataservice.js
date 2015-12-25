c61ae574c4232dd91fcddfbcff974da35ee268e0
Melissa Wilcox <melissa@emdubb.co>
2015-12-24 22:52:56 -0800

    add dataservice for portfolio information

---
+++ js/work.dataservice.js
@@ -0,0 +1,55 @@
+(function(){
+  "use strict"
+
+  angular
+    .module("emdubb")
+    .factory("workDataService", workDataService);
+
+  workDataService.$inject = [];
+
+  function workDataService() {
+    var item1 = {
+      title: "Project1",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+    var item2 = {
+      title: "Project2",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+    var item3 = {
+      title: "Project3",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+    var item4 = {
+      title: "Project4",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+    var item5 = {
+      title: "Project5",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+    var item6 = {
+      title: "Project6",
+      thumbUrl: "/img/thumbs/thumb.jpg",
+      body: "lorem ipsum"
+    }
+
+    var work = [
+      item1,
+      item2,
+      item3,
+      item4,
+      item5,
+      item6
+    ]
+
+    return work
+
+  }
+
+})();
