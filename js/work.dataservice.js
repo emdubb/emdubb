dcf12bb77a163a652dd5a829257bc81858234489
Melissa Wilcox <melissa@emdubb.co>
2016-02-18 13:45:55 -0800

    add id to work items

--- js/work.dataservice.js
+++ js/work.dataservice.js
@@ -9,6 +9,7 @@
 
   function workDataService() {
     var item1 = {
+      id: 1,
       title: "PiRS",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -18,6 +19,7 @@
       print: false
     }
     var item2 = {
+      id: 2,
       title: "Tsunami Campaign",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -27,6 +29,7 @@
       print: true
     }
     var item3 = {
+      id: 3,
       title: "What's Slackin'",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -36,6 +39,7 @@
       print: false
     }
     var item4 = {
+      id: 4,
       title: "Up the River",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -45,6 +49,7 @@
       print: false
     }
     var item5 = {
+      id: 5,
       title: "RAVE Wireless",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -54,6 +59,7 @@
       print: false
     }
     var item6 = {
+      id: 6,
       title: "RAVE Centric",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -63,6 +69,7 @@
       print: false
     }
     var item7 = {
+      id: 7,
       title: "Gorilla Therapy",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -72,6 +79,7 @@
       print: true
     }
     var item8 = {
+      id: 8,
       title: "Say What?",
       thumbUrl: "../img/thumbs/thumb.jpg",
       thumbUrlroll: "../img/thumbs/thumb.jpg",
@@ -81,15 +89,17 @@
       print: true
     }
     var item9 = {
+      id: 9,
       title: "Negretude",
-      thumbUrl: "../img/thumbs/negretude.jpg",
-      thumbUrlroll: "../img/thumbs/negretuderoll.jpg",
+      thumbUrl: "../img/negretude/negretude.jpg",
+      thumbUrlroll: "../img/negretude/negretuderoll.jpg",
       body: "lorem ipsum",
       development: false,
       interactive: false,
       print: true
     }
     var item10 = {
+      id: 10,
       title: "Invisible Children",
       thumbUrl: "../img/thumbs/invisible.jpg",
       thumbUrlroll: "../img/thumbs/invisibleroll.jpg",
